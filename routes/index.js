37cf1782113fab5e2abc9c908ccea011bf027b76
yanhaoqi <yanhaoqi2008@foxmail.com>
2017-11-25 23:25:55 +0800

    根路由的初始页面

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('html', { title: 'Express' });
+    res.sendFile(path.resolve('public/html/index.html'));
 });
 
 module.exports = router;
